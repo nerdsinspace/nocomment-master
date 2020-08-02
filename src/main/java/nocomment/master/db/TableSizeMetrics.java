3dff81c8338c2051ed95d84cb2beaa218e3f38e4
Leijurv <leijurv@gmail.com>
2020-08-01 19:51:19 -0700

    rows too

--- src/main/java/nocomment/master/db/TableSizeMetrics.java
+++ src/main/java/nocomment/master/db/TableSizeMetrics.java
@@ -13,13 +13,19 @@ public class TableSizeMetrics {
             .help("Relation sizes")
             .labelNames("name")
             .register();
+    private static final Gauge relationRows = Gauge.build()
+            .name("relation_rows")
+            .help("Relation rows")
+            .labelNames("name")
+            .register();
 
     public static void update() {
         try (Connection connection = Database.getConnection();
-             PreparedStatement stmt = connection.prepareStatement("SELECT relname AS name, pg_relation_size(C.oid) AS size FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) WHERE nspname NOT IN ('pg_catalog', 'information_schema', 'pg_toast')");
+             PreparedStatement stmt = connection.prepareStatement("SELECT relname AS name, pg_relation_size(C.oid) AS size, reltuples as rows FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) WHERE nspname NOT IN ('pg_catalog', 'information_schema', 'pg_toast')");
              ResultSet rs = stmt.executeQuery()) {
             while (rs.next()) {
                 relationSizes.labels(rs.getString("name")).set(rs.getLong("size"));
+                relationRows.labels(rs.getString("name")).set(rs.getLong("rows"));
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
