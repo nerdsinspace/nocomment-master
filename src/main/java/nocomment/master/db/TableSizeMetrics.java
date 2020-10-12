72122a4e349510526499f5a0d868b30741e3e9b2
Leijurv <leijurv@gmail.com>
2020-10-12 16:28:37 -0700

    fix reindex polluting prometheus table size metrics

--- src/main/java/nocomment/master/db/TableSizeMetrics.java
+++ src/main/java/nocomment/master/db/TableSizeMetrics.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TableSizeMetrics {
     private static final Gauge relationSizes = Gauge.build()
@@ -19,14 +21,27 @@ public class TableSizeMetrics {
             .labelNames("name")
             .register();
 
-    public static void update() {
+    private static final Set<String> tables = new HashSet<>();
+
+    public static synchronized void update() {
         try (Connection connection = Database.getConnection();
              PreparedStatement stmt = connection.prepareStatement("SELECT relname AS name, pg_relation_size(C.oid) AS size, reltuples as rows FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) WHERE nspname NOT IN ('pg_catalog', 'information_schema', 'pg_toast')");
              ResultSet rs = stmt.executeQuery()) {
+            Set<String> seenThisTime = new HashSet<>();
             while (rs.next()) {
-                relationSizes.labels(rs.getString("name")).set(rs.getLong("size"));
-                relationRows.labels(rs.getString("name")).set(rs.getLong("rows"));
+                String name = rs.getString("name");
+                relationSizes.labels(name).set(rs.getLong("size"));
+                relationRows.labels(name).set(rs.getLong("rows"));
+                seenThisTime.add(name);
             }
+            for (String lastTime : tables) {
+                if (!seenThisTime.contains(lastTime)) {
+                    relationSizes.remove(lastTime);
+                    relationRows.remove(lastTime);
+                }
+            }
+            tables.clear();
+            tables.addAll(seenThisTime);
         } catch (SQLException ex) {
             ex.printStackTrace();
             throw new RuntimeException(ex);
