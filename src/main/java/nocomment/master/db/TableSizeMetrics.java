40f8a274b3d4b16fa5facb837402dcacd145a7a0
Leijurv <leijurv@gmail.com>
2021-03-01 00:37:06 -0800

    export prometheus gauge for zfs compressed postgres table size

--- src/main/java/nocomment/master/db/TableSizeMetrics.java
+++ src/main/java/nocomment/master/db/TableSizeMetrics.java
@@ -1,7 +1,11 @@
 package nocomment.master.db;
 
 import io.prometheus.client.Gauge;
+import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
+import shaded.org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,6 +19,11 @@ public class TableSizeMetrics {
             .help("Relation sizes")
             .labelNames("name")
             .register();
+    private static final Gauge relationCompressedSizes = Gauge.build()
+            .name("relation_compressed_sizes")
+            .help("Relation compressed sizes")
+            .labelNames("name")
+            .register();
     private static final Gauge relationRows = Gauge.build()
             .name("relation_rows")
             .help("Relation rows")
@@ -24,19 +33,25 @@ public class TableSizeMetrics {
     private static final Set<String> tables = new HashSet<>();
 
     public static synchronized void update() {
+        Object2LongOpenHashMap<String> compressedSizes = getDiskUsageSummary();
         try (Connection connection = Database.getConnection();
-             PreparedStatement stmt = connection.prepareStatement("SELECT relname AS name, pg_relation_size(C.oid) AS size, reltuples as rows FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) WHERE nspname NOT IN ('pg_catalog', 'information_schema', 'pg_toast')");
+             PreparedStatement stmt = connection.prepareStatement("SELECT relname AS name, pg_relation_size(C.oid) AS size, pg_relation_filepath(C.oid) AS path, reltuples as rows FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) WHERE nspname NOT IN ('pg_catalog', 'information_schema', 'pg_toast')");
              ResultSet rs = stmt.executeQuery()) {
             Set<String> seenThisTime = new HashSet<>();
             while (rs.next()) {
                 String name = rs.getString("name");
                 relationSizes.labels(name).set(rs.getLong("size"));
+                long sz = compressedSizes.getLong(rs.getString("path"));
+                if (sz != 0) {
+                    relationCompressedSizes.labels(name).set(sz);
+                }
                 relationRows.labels(name).set(rs.getLong("rows"));
                 seenThisTime.add(name);
             }
             for (String lastTime : tables) {
                 if (!seenThisTime.contains(lastTime)) {
                     relationSizes.remove(lastTime);
+                    relationCompressedSizes.remove(lastTime);
                     relationRows.remove(lastTime);
                 }
             }
@@ -45,6 +60,16 @@ public class TableSizeMetrics {
         } catch (SQLException ex) {
             ex.printStackTrace();
             throw new RuntimeException(ex);
+        }
+    }
+
+    public static Object2LongOpenHashMap<String> getDiskUsageSummary() {
+        try {
+            Object2LongOpenHashMap<String> map = new Object2LongOpenHashMap<>();
+            IOUtils.readLines(new InputStreamReader(new ProcessBuilder("bash", "-c", "du -s /postgres/base/16385/*").redirectError(ProcessBuilder.Redirect.INHERIT).start().getInputStream())).forEach(line -> map.addTo(line.split("\t")[1].split("\\.")[0].split("/postgres/")[1], Long.parseLong(line.split("\t")[0]) * 1024L));
+            return map;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
